3a1b1993c830a7018bbfeb3ec16b3a8610684268
Garth Bosch <104095101+garth-bosch@users.noreply.github.com>
2024-12-06 11:59:54 +0400

    Expand test and ignored files

--- tests/navigateHome.spec.ts
+++ tests/navigateHome.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 // Test Suite for the sinov8.net page
 test.describe('Sinov8 Page Test', () => {
-  test('should navigate to the home page and check for keywords @end2endtest @regressiontest @nightlytest @smoketest', async ({ page }) => {
+  test('should navigate to the a few pages and check for keywords @end2endtest @regressiontest @nightlytest @smoketest', async ({ page }) => {
     // Open the website
     await page.goto('https://sinov8.net/');
 
@@ -15,5 +15,11 @@ test.describe('Sinov8 Page Test', () => {
       const isVisible = await page.isVisible(`text=${keyword}`);
       expect(isVisible).toBeTruthy();
     }
+
+    await page.getByRole('link', { name: 'Software Solutions' }).click();
+    const page1Promise = page.waitForEvent('popup');
+    await page.getByRole('link', { name: 'Go to our dedicated product' }).click();
+    const page1 = await page1Promise;
+    await page1.getByText('The only end-to-end solution').click();
   });
 });
